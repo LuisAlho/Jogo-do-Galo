0e5e65a554eb8c82045d27e3095cbcdea483a80d
Luis Alho <luisalhopt@gmail.com>
2018-09-04 13:37:31 +0100

    Change system exit to 1

--- src/gestao/Main.java
+++ src/gestao/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String [] args) {
         
+        int port;
+        String url;
+        
         if (args.length == 0){
             System.out.println("Invalid arguments...");
             argumentsHelp();
-            System.exit(0);
+            System.exit(1);
         }
 
         Gestao gestao = new Gestao();
